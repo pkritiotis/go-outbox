f4f9c8cc66593cd3872f90588a9ca09bbf43f2c4
Panayiotis Kritiotis <pkritiotis@gmail.com>
2022-01-29 22:16:23 +0200

    Linter improvements

--- broker_mock.go
+++ broker_mock.go
@@ -2,11 +2,13 @@ package outbox
 
 import "github.com/stretchr/testify/mock"
 
+//MockBroker mocks the Broker interface
 type MockBroker struct {
 	mock.Mock
 }
 
-func (m MockBroker) Send(message Message) *BrokerError {
+//Send method mock
+func (m *MockBroker) Send(message Message) *BrokerError {
 	args := m.Called(message)
 	return args.Get(0).(*BrokerError)
 }
