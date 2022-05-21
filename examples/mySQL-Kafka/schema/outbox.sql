6c1f86aef8677684fc6788e2e3bb494bdf7580ab
Panayiotis Kritiotis <pkritiotis@gmail.com>
2022-05-21 10:46:30 +0300

    Added the correct database schema for the mySQL-Kafka example

--- examples/mySQL-Kafka/schema/outbox.sql
+++ examples/mySQL-Kafka/schema/outbox.sql
@@ -5,5 +5,8 @@ CREATE TABLE outbox (
         created_on DATETIME NOT NULL,
         locked_by varchar(100) NULL,
         locked_on DATETIME NULL,
-        processed_on DATETIME NULL
+        processed_on DATETIME NULL,
+        number_of_attempts INT NOT NULL,
+        last_attempted_on DATETIME NULL,
+        error varchar(1000) NULL
 )
